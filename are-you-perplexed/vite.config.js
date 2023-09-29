14871912c76659bd2f2af3648b1afb5ab8b5c3f1
jvredmoore <jvredmoore@gmail.com>
2023-09-29 16:44:13 -0400

    add deploy workflow

--- are-you-perplexed/vite.config.js
+++ are-you-perplexed/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    base: '/whatmorecouldyouneed.github.io/',
+    base: '/whatmorecouldyouneed.github.io/are-you-perplexed',
   },
 });
