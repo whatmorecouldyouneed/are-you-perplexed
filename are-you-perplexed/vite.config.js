f3a0bc1d94a8513d006569328f13921796412582
jvredmoore <jvredmoore@gmail.com>
2023-09-30 15:26:52 -0400

    add deploy workflow

--- are-you-perplexed/vite.config.js
+++ are-you-perplexed/vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    base: '/whatmorecouldyouneed.github.io/',
+    base: '/whatmorecouldyouneed.github.io',
   },
 });
